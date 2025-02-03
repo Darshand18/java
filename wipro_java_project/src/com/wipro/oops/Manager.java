69befbcb053e17b82ef90422109098e454fbc2d0
Darshan D <dd656804@gmail.com>
2025-02-03 12:22:47 +0530

    Day2

--- wipro_java_project/src/com/wipro/oops/Manager.java
+++ wipro_java_project/src/com/wipro/oops/Manager.java
@@ -16,5 +16,19 @@ public class Manager extends Employee {
 	public Manager() {
 		// TODO Auto-generated constructor stub
 	}
+	public static void main(String a[]) {
+		//child object is instantiated from child constructor
+	    //parent class consumes the properties and behaviors of child class
+		Employee emp = new Manager();
+		emp.setId(24);
+		emp.setName("Darshan");
+		emp.setDepartment("Angular");
+		emp.setSalary(30000f);
+		
+		System.out.println(emp.getId());;
+		System.out.println(emp.getName());
+		System.out.println(emp.getDepartment());
+		System.out.println(emp.getSalary());
+	}
 
 }
